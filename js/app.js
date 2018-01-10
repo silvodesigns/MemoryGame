0b924c51efcd455f752bea84c425bdbf7e298a25
Kevin Silvestre <keke1616@gmail.com>
2018-01-09 23:46:26 -0500

    Update
    The game is fullly playable this time around

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 /*
  * Create a list that holds all of your cards
  */
- const Cards = document.getElementsByClassName('card');
- const openCards =[];
- const matchCards =[];
+const Cards = document.getElementsByClassName('card');
+const openCards = [];
+const matchCards = [];
 
 
 
@@ -18,7 +18,8 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -33,33 +34,35 @@ function shuffle(array) {
 
 //add event listener to reset/ shuffle button
 const reset = document.getElementById("reset");
-const deck =  document.getElementById("deck");//parent element
+const deck = document.getElementById("deck"); //parent element
 
 
-	reset.addEventListener('click',function(){
-		console.log("clicked");
-		for(i=0; i < Cards.length; i++){
+reset.addEventListener('click', function() {
+    console.log("clicked");
+    for (i = 0; i < Cards.length; i++) {
 
-			Cards[i].classList.remove("open", "show");
-		}
+        Cards[i].classList.remove("match", "ani"); //remove these classes when cards are shuffled
+        Cards[i].classList.remove("open", "show");
 
-		const shuffled = shuffle(Cards);
-		for(i=0; i < shuffled.length; i++){
-			deck.appendChild(shuffled[i]);
-		}
+    }
+
+    const shuffled = shuffle(Cards);
+    for (i = 0; i < shuffled.length; i++) {
+        deck.appendChild(shuffled[i]);
+    }
 
 
-		const moves = document.getElementById("moves");
-		moves.textContent ="0";
-	});
+    const moves = document.getElementById("moves");
+    moves.textContent = "0";
+});
 
 //Update moves on the board
-function updateMoves(score){
+function updateMoves(score) {
 
-	const moves = document.getElementById("moves");
-	const movesValue = moves.textContent;
-	const newValue = parseInt(score) + parseInt(movesValue);
-	moves.innerHTML = newValue;
+    const moves = document.getElementById("moves");
+    const movesValue = moves.textContent;
+    const newValue = parseInt(score) + parseInt(movesValue);
+    moves.innerHTML = newValue;
 }
 
 /*
@@ -74,67 +77,96 @@ function updateMoves(score){
  */
 
 //displaySymbol function displays the card's symbol when it is opened
- function displaySymbol(card){
+function displaySymbol(card) {
 
- 	card.classList.add("open", "show");
+    card.classList.add("open", "show");
 
 
- }
+}
 
- function won(){
+//won function displays a screen to the user after the game has been won
+function won() {
 
- 	if(matchCards.length == 16){
- 		const screen = document.getElementsByClassName("win");
- 		screen[0].style.display = "block";
- 	}
+    if (matchCards.length == 16) {
+        const screen = document.getElementsByClassName("win");
+        screen[0].style.display = "block";
+    }
 
- }
+}
+
+// is fired when the users clicks on the play again button
+const button = document.getElementById("play");
+
+button.addEventListener('click', function() {
+    for (i = 0; i < Cards.length; i++) {
+
+        Cards[i].classList.remove("match", "ani"); //remove these classes when cards are shuffled
+        Cards[i].classList.remove("open", "show");
+
+    }
+
+    const shuffled = shuffle(Cards);
+    for (i = 0; i < shuffled.length; i++) {
+        deck.appendChild(shuffled[i]);
+    }
+
+
+    const moves = document.getElementById("moves");
+    moves.textContent = "0";
+
+
+    const screen = document.getElementsByClassName("win");
+    screen[0].style.display = "none";
+});
+
+
 
 
 // AddToList function adds the cliked cards into an array temporarily to check weather the clicked cards match or no
- function addToList(card){
- 	
- 	//pushs the clickec card into the openCards array
- 	openCards.push(card);
- 	//if the array's length is less than 2.
- 	if(openCards.length > 1 ){
+function addToList(card) {
 
- 					//check for the classes on both cards to check if the symbol is the same.
- 					if(openCards[0].childNodes[1].classList[1] == openCards[1].childNodes[1].classList[1]){
- 						console.log("they are the same");
- 						//if they are the same lets remove it from the openCards array to the matchCards array
- 						matchCards.push(openCards[0]);//change colors of cards if they match
- 						matchCards.push(openCards[1]);//change colors of cards if they match
- 						openCards.length = 0;
- 						updateMoves("1");
- 						won();
+    //pushs the clickec card into the openCards array
+    openCards.push(card);
+    //if the array's length is less than 2.
+    if (openCards.length > 1) {
 
-
- 					} else{
- 						console.log("they are not the same");
- 						// if they do not match lets hide the cards on the table and set a little timer before it happens
- 						setTimeout( function(){
- 							openCards[0].classList.remove("open", "show");
- 							openCards[1].classList.remove("open", "show");
- 							openCards.length = 0;
- 							updateMoves("1");
+        //check for the classes on both cards to check if the symbol is the same.
+        if (openCards[0].childNodes[1].classList[1] == openCards[1].childNodes[1].classList[1]) {
+            console.log("they are the same");
+            //if they are the same lets remove it from the openCards array to the matchCards array
+            matchCards.push(openCards[0]);
+            matchCards.push(openCards[1]);
+            openCards[0].classList.add("match", "ani");
+            openCards[1].classList.add("match", "ani");
+            openCards.length = 0;
+            updateMoves("1");
+            won();
 
 
- 						},1000);
-
- 							
- 					}
- 				}
- 
- }
-// Add event listener to each of the cards
-for(i = 0; i < Cards.length; i++){
- 		Cards[i].addEventListener('click', function(){
-
- 				displaySymbol(this);
- 				addToList(this);
+        } else {
+            console.log("they are not the same");
+            // if they do not match lets hide the cards on the table and set a little timer before it happens
+            setTimeout(function() {
+                openCards[0].classList.remove("open", "show");
+                openCards[1].classList.remove("open", "show");
+                openCards.length = 0;
+                updateMoves("1");
 
 
-		 });
+            }, 1000);
+
+
+        }
+    }
+
 }
+// Add event listener to each of the cards
+for (i = 0; i < Cards.length; i++) {
+    Cards[i].addEventListener('click', function() {
 
+        displaySymbol(this);
+        addToList(this);
+
+
+    });
+}
