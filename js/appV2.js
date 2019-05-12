a45a9f01cd64c191ee9a00c0f73b2587ea04b947
Kevin-S <kevsilvo@gmail.com>
2019-05-12 04:44:53 -0400

    v2

--- js/appV2.js
+++ js/appV2.js
@@ -1,13 +1,26 @@
-class pokeBalls{
+class PokeBalls{
     constructor(pokeCard){
         this.pokeCard = pokeCard;
         this.pokeType = pokeCard.dataset.ball;
 
     }
 
+    displaySymbol(){
+
+        this.pokeCard.classList.add("open");
+        this.pokeCard.children[0].classList.remove('hide-it');
+    }
+
+    addToList(){
+        return new Compare(this.pokeCard);
+    }
 
 
+   // this.pokeCard.addEventListener('click', () => this.displaySymbol());
 
+}
+
+class Compare(){
 
 
 }
