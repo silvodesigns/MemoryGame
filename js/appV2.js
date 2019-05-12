fcbc4b0d920c9404318a670169920995d1a66633
Kevin-S <kevsilvo@gmail.com>
2019-05-12 05:08:10 -0400

    v3

--- js/appV2.js
+++ js/appV2.js
@@ -2,6 +2,8 @@ class PokeBalls{
     constructor(pokeCard){
         this.pokeCard = pokeCard;
         this.pokeType = pokeCard.dataset.ball;
+        this.opened = [];
+        this.matched = [];
 
     }
 
@@ -12,7 +14,32 @@ class PokeBalls{
     }
 
     addToList(){
-        return new Compare(this.pokeCard);
+        this.opened.push(this.pokeCard);
+        if(this.opened.length > 1){
+             opened.splice(2, 1);
+             this.compare();
+
+        }    
+    }
+
+
+    compare(){
+
+        if(this.opened[0].dataset.ball == this.opened[1].dataset.ball){
+            console.log("they are the same");
+            if (this.matched.indexOf(this.opened[0]) > -1 && this.matched.indexOf(this.opened[1]) > -1) {
+                console.log("dont push");
+            } else {
+
+                this.opened.forEach(function(element){
+                        matchedCards.push(element);
+                })
+                
+            }
+
+        }
+
+
     }
 
 
@@ -20,7 +47,5 @@ class PokeBalls{
 
 }
 
-class Compare(){
 
 
-}
