81703b6704e778f84315b0f98fc368f3de3ec0b1
silvodesigns <kevsilvo@gmail.com>
2019-09-04 07:49:15 -0400

    building w react

---
+++ game/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import GameBoard from './components/GameBoard/GameBoard.js';
+import GameHeader from './components/GameHeader/GameHeader.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GameHeader />
+      <GameBoard />
+    </div>
+  );
+}
+
+export default App;
---
+++ game/src/components/GameHeader/GameHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function GameHeader() {
+    return (
+
+        <div className="game-header">
+            <p>This is the header</p>
+        </div>
+
+    );
+}
+
+export default GameHeader;
