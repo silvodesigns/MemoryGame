a8670ed88db43e7a3f1101183071fc6574383715
Kevin Silvestre <kevsilvo@gmail.com>
2020-07-21 14:10:48 -0400

    added full width and height to main screen

--- src/components/GameControls/AudioControl.js
+++ src/components/GameControls/AudioControl.js
@@ -4,14 +4,12 @@ import './audio-control.css'
 const Audio = props => {
 
     return (
-
         <div className="audio-control">
             <audio autoPlay="autoplay" loop="loop">
                 <source src={require('../../audio/theme.mp3')} type="audio/mp3" />
                 Your browser does not support the audio element.
             </audio>
         </div>
-
     );
 }
 
