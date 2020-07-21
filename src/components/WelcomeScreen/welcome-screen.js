52b7cf51973480e73e759fa1186774a0111ca1b8
Kevin Silvestre <kevsilvo@gmail.com>
2020-07-21 13:43:51 -0400

    new colors added yo UI

--- src/components/WelcomeScreen/welcome-screen.js
+++ src/components/WelcomeScreen/welcome-screen.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import Button from "../buttons/button.js";
 import './welcomeScreen.css'
 
 
 function WelcomeScreen() {
     return (
         <div className="welcome-screen">
-            <h1>Welcome to the Game</h1>
+            <div class="wrapper">
+                <img class="wc-logo" src={require("./img/pklogo.png")} />
+                <h1>MATCH THEM ALL</h1>
+                <Button />
+            </div>
         </div>
     );
 }
---
+++ src/components/buttons/button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './button.css';
+
+const Button = props => {
+
+    return (
+
+        <div className="button">
+            <p>PLAY NOW</p>
+        </div>
+
+    );
+}
+
+export default Button;
