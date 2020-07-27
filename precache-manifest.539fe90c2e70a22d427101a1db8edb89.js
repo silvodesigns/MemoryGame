93325f3387dbcb775ac273ec404026de4ff64e58
Kevin Silvestre <kevsilvo@gmail.com>
2020-07-27 02:58:16 -0400

    Updates

---
+++ precache-manifest.539fe90c2e70a22d427101a1db8edb89.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cdf0a086c633ec06928c68bc0c48186",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "a91451bf9c5acbb5ccdc",
+    "url": "/MemoryGame/static/css/main.4b520c47.chunk.css"
+  },
+  {
+    "revision": "6418689b23c060cb9c98",
+    "url": "/MemoryGame/static/js/2.2b1a139e.chunk.js"
+  },
+  {
+    "revision": "a91451bf9c5acbb5ccdc",
+    "url": "/MemoryGame/static/js/main.79f0bb88.chunk.js"
+  },
+  {
+    "revision": "2eae902c681edc6ef09a",
+    "url": "/MemoryGame/static/js/runtime~main.9665e191.js"
+  },
+  {
+    "revision": "e3c1428be6ba7dd2b83cb10126a8696a",
+    "url": "/MemoryGame/static/media/ball0.e3c1428b.png"
+  },
+  {
+    "revision": "219126d0fdc47cdbcf2524075516e74f",
+    "url": "/MemoryGame/static/media/ball1.219126d0.png"
+  },
+  {
+    "revision": "20ff19370abba3393d963d2fd109f3e7",
+    "url": "/MemoryGame/static/media/ball2.20ff1937.png"
+  },
+  {
+    "revision": "80bd7ed869059a2c5c98f7e8035a902e",
+    "url": "/MemoryGame/static/media/ball3.80bd7ed8.png"
+  },
+  {
+    "revision": "16bfd5118bbea7c9fb15cad7cbb8a536",
+    "url": "/MemoryGame/static/media/ball4.16bfd511.png"
+  },
+  {
+    "revision": "12a883e3727e20e07e837378063fce9e",
+    "url": "/MemoryGame/static/media/ball5.12a883e3.png"
+  },
+  {
+    "revision": "80511eb24a278ce5028127330edab78b",
+    "url": "/MemoryGame/static/media/ball6.80511eb2.png"
+  },
+  {
+    "revision": "ef78a925cd40227aefa96d7572b1882e",
+    "url": "/MemoryGame/static/media/ball7.ef78a925.png"
+  },
+  {
+    "revision": "8859e86fccefbfddeb4b28a047639ab4",
+    "url": "/MemoryGame/static/media/pklogo.8859e86f.png"
+  },
+  {
+    "revision": "3b8089534575bc485c203d4fcb46109d",
+    "url": "/MemoryGame/static/media/star-filled.3b808953.svg"
+  },
+  {
+    "revision": "88688cf0265e297262d0cf7470c1609c",
+    "url": "/MemoryGame/static/media/star.88688cf0.svg"
+  },
+  {
+    "revision": "8520e509b865f6c37611eb381f85f2a8",
+    "url": "/MemoryGame/static/media/theme.8520e509.mp3"
+  }
+]);
